a722e91b60b0bc1ad8e0026bf89ec0388d1f9451
Quokka <everestfesf@gmail.com>
2025-03-14 08:07:21 +0900

    Time: 0 ms (100%), Space: 10.6 MB (94.94%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string source) {
+  
+        bool table[128] = { false, };
+        
+        int maxCount = 0, count = 0;  
+        int left = 0, right = 0;
+        int length = source.size();
+        while (right < length) {
+            
+            char character = source[right];
+            if (table[(int)character] == 0) {
+            
+            table[(int)character] = true;
+            right++;
+            count++;
+            maxCount = max (maxCount, count);
+            continue;
+            }
+            while (source[left++] != character) {
+                
+            table[(int)source[left - 1]] = false;
+            count--;
+            }
+            
+            right++;
+            table[(int)character] = true;
+            maxCount = max (maxCount, count);
+        }
+        return maxCount;
+    }
+};
