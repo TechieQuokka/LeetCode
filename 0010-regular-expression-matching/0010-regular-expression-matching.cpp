694686b52c38f3b04fbfc0fdc119aaf308f045c8
Ryou <everestfesf@gmail.com>
2026-01-03 10:37:47 +0900

    Time: 0 ms (100%), Space: 9.1 MB (50.04%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) { return isMatchRecursive(s, p, 0, 0); }
+    bool isMatch(string s, string p) {
 
-    bool isMatchRecursive(string source, string pattern, int s_index,
-                          int p_index) {
+        vector<vector<optional<bool>>> memo (s.size()+1, vector<optional<bool>>(p.size()+1));
+        return isMatchRecursive(memo, s, p, 0, 0);
+    }
+
+    bool isMatchRecursive(vector<vector<optional<bool>>>& memo, const string& source, const string& pattern, int s_index, int p_index) {
 
         if (s_index == source.size() && p_index == pattern.size())
             return true;
         if (p_index == pattern.size())
             return false;
 
-        bool firstMatch =
-            (s_index < source.size()) &&
-            (pattern[p_index] == source[s_index] || pattern[p_index] == '.');
+        if (memo[s_index][p_index].has_value()) return memo[s_index][p_index].value();
+
+        bool firstMatch = (s_index < source.size()) && (pattern[p_index] == source[s_index] || pattern[p_index] == '.');
 
         if (p_index + 1 < pattern.size() && pattern[p_index + 1] == '*') {
 
-            bool result =
-                isMatchRecursive(source, pattern, s_index, p_index + 2);
-            result |= firstMatch &&
-                      isMatchRecursive(source, pattern, s_index + 1, p_index);
+            bool result = isMatchRecursive(memo, source, pattern, s_index, p_index + 2);
+            result |= firstMatch && isMatchRecursive(memo, source, pattern, s_index + 1, p_index);
+            memo[s_index][p_index] = result;
             return result;
-        } else
-            return firstMatch &&
-                   isMatchRecursive(source, pattern, s_index + 1, p_index + 1);
+        } 
+        memo[s_index][p_index] = firstMatch && isMatchRecursive(memo, source, pattern, s_index + 1, p_index + 1);
+        return memo[s_index][p_index].value();
     }
 };
