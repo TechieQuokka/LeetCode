8e300a95660ecd5a4c75d97f6ba78ad4d41f36c7
Ryou <everestfesf@gmail.com>
2026-01-09 11:33:55 +0900

    Time: 19 ms (39.86%), Space: 18.4 MB (40.31%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -10,25 +10,27 @@ public:
 
 private:
     bool isScrambleRecursive(string source1, string source2) {
+
+        if (table.contains(source1) && table[source1].contains(source2)) return table[source1][source2];
         
-        if (source1 == source2) return true;
-        if (source1.length() != source2.length()) return false;
+        if (source1 == source2) return table[source1][source2] = true;
+        if (source1.length() != source2.length()) return table[source1][source2] = false;
 
         string temp1 = source1, temp2 = source2;
         sort (temp1.begin(), temp1.end());
         sort (temp2.begin(), temp2.end());
-        if (temp1 != temp2) return false;
+        if (temp1 != temp2) return table[source1][source2] = false;
 
         int length = source1.size();
         for (int index = 1; index < length; index++) {
-            if (isScrambleRecursive(substring (source1, 0, index - 1), substring (source2, 0, index - 1))
-             && isScrambleRecursive(substring (source1, index, length - 1), substring(source2, index, length - 1))) return true;
+            if ((table[source1][source2] = isScrambleRecursive(substring (source1, 0, index - 1), substring (source2, 0, index - 1)))
+             && (table[source1][source2] = isScrambleRecursive(substring (source1, index, length - 1), substring(source2, index, length - 1)))) return true;
 
-            if (isScrambleRecursive(substring (source1, 0, index - 1), substring (source2, length - index, length - 1))
-             && isScrambleRecursive(substring (source1, index, length - 1), substring(source2, 0, length - index - 1))) return true;
+            if ((table[source1][source2]= isScrambleRecursive(substring (source1, 0, index - 1), substring (source2, length - index, length - 1)))
+             && (table[source1][source2]= isScrambleRecursive(substring (source1, index, length - 1), substring(source2, 0, length - index - 1)))) return true;
         }
 
-        return false;
+        return table[source1][source2] = false;
     }
 
     string substring (string source, size_t start, size_t end) {
